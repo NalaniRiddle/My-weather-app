66ab12baa74cdcd2198a5acad1dce6b7d3ffc4d6
NalaniRiddle <120990843+NalaniRiddle@users.noreply.github.com>
2023-01-22 22:49:31 -0700

    cel to fer conversion

--- script.js
+++ script.js
@@ -25,8 +25,9 @@ function formatDate(timestamp) {
 
 /// Display
 function showWeather(response) {
-  let tempElement = document.querySelector("#main-temp");
-  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+  celsiusTemperature = response.data.main.temp;
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -40,7 +41,7 @@ function showWeather(response) {
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
 
-  let dateElement = document.querySelector("#date");
+  let dateElement = document.querySelector("#todays-date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
   let iconElement = document.querySelector("#icon");
@@ -48,27 +49,50 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(city){
+/// search
+function search(city) {
   let apiKey = `369496079cb0740e8e8eb63feb92a9f7`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showWeather);
 }
 
-/// Name of the city -> search
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-city");
   search(cityInput.value);
 }
 
-
 search("Phoenix");
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let celsiusTemperature = null;
 
 /// current geo - lat and lon
 function currentLocation(position) {
@@ -94,4 +118,3 @@ function weatherLocation(response) {
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
 }
-
