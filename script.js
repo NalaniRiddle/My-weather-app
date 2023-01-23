bf09496e257b84f8f3ae645fe2266be45ea5e89a
NalaniRiddle <120990843+NalaniRiddle@users.noreply.github.com>
2023-01-22 22:16:17 -0700

    search update

--- script.js
+++ script.js
@@ -23,52 +23,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-/// Name of the city -> search
-
-function cityName(event) {
-  event.preventDefault();
-  let cityDisplay = document.querySelector("#search-city");
-  let currentCity = document.querySelector("#city");
-  if (cityDisplay.value) {
-    currentCity.innerHTML = cityDisplay.value;
-  } else {
-    currentCity.innerHTML = null;
-    alert("Please type a city");
-    currentCity.innerHTML = "Nowhere";
-  }
-  let apiKey = `369496079cb0740e8e8eb63feb92a9f7`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityDisplay.value}&units=imperial&appid=${apiKey}`;
-  axios.get(apiUrl).then(weatherLocation);
-}
-let city = document.querySelector("#city-search");
-city.addEventListener("submit", cityName);
-
-/// current geo - lat and lon
-function currentLocation(position) {
-  let apiKey = "369496079cb0740e8e8eb63feb92a9f7";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  axios.get(apiUrl).then(showWeather);
-}
-
-function getcurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(currentLocation);
-}
-let locationButton = document.querySelector("#current-location");
-locationButton.addEventListener("click", getcurrentLocation);
-
-/// changing the city
-function weatherLocation(response) {
-  let searchTemp = document.querySelector("#main-temp");
-  let temperature = Math.round(response.data.main.temp);
-  searchTemp.innerHTML = `${temperature}`;
-  let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].description;
-}
-
-/// Display - city
+/// Display
 function showWeather(response) {
   let tempElement = document.querySelector("#main-temp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
@@ -95,24 +50,48 @@ function showWeather(response) {
   );
 }
 
-/// Celsius to Farenheit
-
-function CelusToFarenheit(event) {
-  event.preventDefault();
-  let temperature = document.querySelector("#main-temp");
-  let farenheitTemp = temperature.innerHTML;
-  temperature.innerHTML = Math.round((farenheitTemp * 9) / 5 + 32);
+function search(city){
+  let apiKey = `369496079cb0740e8e8eb63feb92a9f7`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
 }
-let farenheit = document.querySelector("#fahrenheit-link");
-farenheit.addEventListener("click", CelusToFarenheit);
 
-/// Farenheit to Celsius
-
-function FarenheitToCelsius(event) {
+/// Name of the city -> search
+function handleSubmit(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#main-temp");
-  let celsiusTemp = temperature.innerHTML;
-  temperature.innerHTML = `21`;
+  let cityInput = document.querySelector("#search-city");
+  search(cityInput.value);
 }
-let celsius = document.querySelector("#celsius-link");
-celsius.addEventListener("click", FarenheitToCelsius);
+
+
+search("Phoenix");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit)
+
+
+/// current geo - lat and lon
+function currentLocation(position) {
+  let apiKey = "369496079cb0740e8e8eb63feb92a9f7";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getcurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+let locationButton = document.querySelector("#current-location");
+locationButton.addEventListener("click", getcurrentLocation);
+
+/// changing the city
+function weatherLocation(response) {
+  let searchTemp = document.querySelector("#main-temp");
+  let temperature = Math.round(response.data.main.temp);
+  searchTemp.innerHTML = `${temperature}`;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
