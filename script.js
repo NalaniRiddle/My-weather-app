9d7b96192c438225486c038e21bcd7da4db1fca6
NalaniRiddle <120990843+NalaniRiddle@users.noreply.github.com>
2023-01-22 21:40:52 -0700

    API icon

--- script.js
+++ script.js
@@ -72,16 +72,27 @@ function weatherLocation(response) {
 function showWeather(response) {
   let tempElement = document.querySelector("#main-temp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+
   let description = document.querySelector("#decription");
   description.innerHTML = response.data.weather[0].description;
+
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
+
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 /// Celsius to Farenheit
