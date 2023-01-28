d08c12a7db920cdf4a44e00eb21bd79a1d9bcd8e
NalaniRiddle <120990843+NalaniRiddle@users.noreply.github.com>
2023-01-27 23:10:30 -0700

    code touch up

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function formatDay(timestamp) {
 
 /// weather display
 function showWeather(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -39,7 +40,6 @@ function showWeather(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
@@ -50,7 +50,7 @@ function showWeather(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
@@ -63,7 +63,6 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 ///JS for weekly forecast
 function displayForecast(response) {
   let forecast = response.data.daily;
@@ -144,4 +143,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Phoenix");
+search("London");
